8d469a86b5a95596d78c3dca04a15522e31c16f5
Igor Borodikhin <iborodikhin@gmail.com>
2013-07-14 13:52:32 +0600

    Added composer.json and moved all files to subdirectory

---
+++ src/Glue/Storage/Blob.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * User: majesty
+ * Date: 14.07.13
+ */
+
+namespace Glue\Storage;
+
+
+class Blob extends AbstractFile
+{
+    /**
+     * File extension
+     *
+     * @var string
+     */
+    protected $extension = 'dat';
+
+    /**
+     * Writes data to BLOB
+     *
+     * @param $data
+     * @return array
+     */
+    public function save($data)
+    {
+        $fh = $this->open();
+        flock($fh, LOCK_EX);
+        $offset = $this->size();
+        fseek($fh, $offset);
+        $result = fwrite($fh, $data);
+        flock($fh, LOCK_UN);
+
+        if ($result === false) {
+            return false;
+        }
+
+        return array($offset, $result);
+    }
+
+    /**
+     * Reads data from BLOB.
+     *
+     * @param $offset
+     * @param $length
+     * @return string
+     */
+    public function read($offset, $length)
+    {
+        $fh = $this->open();
+        flock($fh, LOCK_SH);
+        fseek($fh, $offset);
+        $result = fread($fh, $length);
+        flock($fh, LOCK_UN);
+
+        return $result;
+    }
+
+    /**
+     * Deletes data from BLOB
+     *
+     * @param $offset
+     * @param $length
+     * @return bool
+     */
+    public function delete($offset, $length)
+    {
+        $fh = $this->open();
+        flock($fh, LOCK_EX);
+        fseek($fh, $offset);
+        $result = fwrite($fh, str_repeat(chr(0), $length));
+        flock($fh, LOCK_UN);
+
+        if ($result === false) {
+            return false;
+        }
+
+        return true;
+    }
+}
